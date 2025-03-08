f6cb3390f0e64c657953a32dfe4a50dcde615d18
sableor <68871965+sableor@users.noreply.github.com>
2025-03-08 15:53:05 -0500

    new price

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,20 +1,55 @@
 (function () {
-  var ImageMenu = document.querySelector("#mainmenu");
+  var ImageMenu1 = document.querySelector("#mainmenu1");
+  var ImageMenu2 = document.querySelector("#mainmenu2");
+  var ImageMenu3 = document.querySelector("#mainmenu3");
+  var ImageMenu4 = document.querySelector("#mainmenu4");
+  var ImageMenu5 = document.querySelector("#mainmenu5");
+  var ImageMenu6 = document.querySelector("#mainmenu6");
 
-  ImageMenu.addEventListener(
+  ImageMenu1.addEventListener(
     "click",
     function () {
-      ImageMenu.classList.toggle("lightBox");
+      ImageMenu1.classList.toggle("lightBox");
     },
     false
   );
-
-  var ImageMenu2 = document.querySelector("#mainmenu2");
 
   ImageMenu2.addEventListener(
     "click",
     function () {
       ImageMenu2.classList.toggle("lightBox");
+    },
+    false
+  );
+
+  ImageMenu3.addEventListener(
+    "click",
+    function () {
+      ImageMenu3.classList.toggle("lightBox");
+    },
+    false
+  );
+
+  ImageMenu4.addEventListener(
+    "click",
+    function () {
+      ImageMenu4.classList.toggle("lightBox");
+    },
+    false
+  );
+
+  ImageMenu5.addEventListener(
+    "click",
+    function () {
+      ImageMenu5.classList.toggle("lightBox");
+    },
+    false
+  );
+
+  ImageMenu6.addEventListener(
+    "click",
+    function () {
+      ImageMenu6.classList.toggle("lightBox");
     },
     false
   );
